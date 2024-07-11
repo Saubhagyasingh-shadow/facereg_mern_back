9f3f67bee55225a7a6083b6e2191c6c33e6584b2
Saubhagya321 <kavitasachan15k@gmail.com>
2024-07-11 11:36:21 +0530

    Fs

--- server.js
+++ server.js
@@ -1,40 +1,38 @@
-// import { v2 as cloudinary } from 'cloudinary'
 const express = require("express");
-const bodyParser = require("body-parser"); // Require body-parser
-
-const app = express();
+const bodyParser = require("body-parser");
+const cors = require('cors');
 const path = require("path");
-const fileupload= require('express-fileupload')
+const fileupload = require('express-fileupload');
 const hbs = require('express-handlebars');
 
-// //for cloudinary
+const app = express();
+
+// Enable CORS for all origins
+app.use(cors());
+
+// For Cloudinary (commented out sections, if needed, can be uncommented)
 // const cloudinary = require('cloudinary').v2;
-// //this is cloudinary config
 // cloudinary.config({ 
 //     cloud_name: 'dhadmzvzt', 
 //     api_key: '136562861254861', 
 //     api_secret: 'dMMZu2VvDo9JcCKONJs-HWfIEnU'
-//   });
-
-
-
+// });
 
 app.use(express.json());
-// Set maximum payload size to 50 MB
+// Set maximum payload size to 100 MB
 app.use(bodyParser.json({ limit: '100mb' }));
 app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
-
 
 app.use(express.static(path.join(__dirname, "public")));
 
 require('./server/database/database')();
 
-////for connecting with cloudaniry
+// For connecting with Cloudinary
 app.use(fileupload({
-    useTempFiles : true,
+    useTempFiles: true,
 }));
 
-
+// View engine setup (if needed)
 // app.set('view engine', 'hbs');
 // app.engine('hbs', hbs.engine({
 //     extname: 'hbs',
@@ -46,5 +44,5 @@ app.use(fileupload({
 app.use('/', require('./server/router/router'));
 
 app.listen(3000, () => {
-    console.log("server is running on port 3000");
-})
+    console.log("Server is running on port 3000");
+});
